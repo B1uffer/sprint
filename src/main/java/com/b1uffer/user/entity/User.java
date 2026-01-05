9abbaa96f8d679271dff46028b338fb11c2d9814
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-01-05 14:41:24 +0900

    User 생성자를 조건에 따라 만듬

--- src/main/java/com/b1uffer/user/entity/User.java
+++ src/main/java/com/b1uffer/user/entity/User.java
@@ -2,5 +2,22 @@ package com.b1uffer.user.entity;
 
 import com.b1uffer.base.BaseEntity;
 
+import java.time.Instant;
+import java.util.UUID;
+
 public class User extends BaseEntity {
+    // 이름
+    private String name;
+
+    // 비밀번호
+    private String password;
+
+    // 설명
+    private String description;
+
+    public User(UUID id, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
