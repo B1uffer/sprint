807053b384bf6f61b03eefce1274baa002c2b41c
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-01-05 14:50:08 +0900

    Message 필드, setter, getter 만듬

--- src/main/java/com/b1uffer/message/entity/Message.java
+++ src/main/java/com/b1uffer/message/entity/Message.java
@@ -1,6 +1,44 @@
 package com.b1uffer.message.entity;
 
 import com.b1uffer.base.BaseEntity;
+import com.b1uffer.user.entity.User;
+
+import java.time.Instant;
+import java.util.UUID;
 
 public class Message extends BaseEntity {
+    /**
+     * 메시지 내용
+     */
+    public String text;
+
+    /**
+     * 메세지 쓴놈
+     */
+    public User user;
+
+    /**
+     * setter
+     */
+    public Message(UUID id, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Message(String text, User user) {
+        this.text = text;
+        this.user = user;
+    }
+
+    /**
+     * getter
+     */
+    public String getText() {
+        return text;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
