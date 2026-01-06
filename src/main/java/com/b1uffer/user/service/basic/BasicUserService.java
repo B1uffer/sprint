de11cfe1c11772fe7cedb01e5fec2eda12b4b0c7
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-01-06 14:22:56 +0900

    BasicUserService read 구현, update 매개변수 수정

--- src/main/java/com/b1uffer/user/service/basic/BasicUserService.java
+++ src/main/java/com/b1uffer/user/service/basic/BasicUserService.java
@@ -3,9 +3,13 @@ package com.b1uffer.user.service.basic;
 import com.b1uffer.user.entity.User;
 import com.b1uffer.user.service.UserService;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class BasicUserService implements UserService {
+    private final Map<UUID, User> userRepository = new HashMap<>();
+
     @Override
     public User create(String name, String password, String description, String image) {
         if(name.isEmpty()) {
@@ -25,17 +29,23 @@ public class BasicUserService implements UserService {
         }
 
         User user = new User(name, password, description, image);
+        userRepository.put(user.getId(), user);
 
         return user;
     }
 
     @Override
     public User read(UUID userId) {
-        return null;
+        if(userId == null) {
+            throw new IllegalArgumentException("userId cannot be null");
+        }
+
+        User user = userRepository.get(userId);
+        return user;
     }
 
     @Override
-    public User update(String name, String password, String description, String image) {
+    public User update(UUID id, String name, String password, String description, String image) {
         return null;
     }
 
