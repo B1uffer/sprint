b139785abbb73337e47ae1978bcb24a9ba8972ed
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-01-06 16:12:06 +0900

    MessageService 인터페이스 구현, create 메서드 생성

---
+++ src/main/java/com/b1uffer/message/service/MessageService.java
@@ -0,0 +1,9 @@
+package com.b1uffer.message.service;
+
+import com.b1uffer.message.entity.Message;
+
+import java.util.UUID;
+
+public interface MessageService {
+    Message create(String text, UUID userId);
+}
