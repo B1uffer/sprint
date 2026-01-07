d473e805a0a3d98218d760d46f13221aaa9e1c2f
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-01-07 12:54:42 +0900

    ChannelService의 create 메서드 구현

--- src/main/java/com/b1uffer/channel/service/BasicChannelService.java
+++ src/main/java/com/b1uffer/channel/service/BasicChannelService.java
@@ -1,4 +1,25 @@
 package com.b1uffer.channel.service;
 
+import com.b1uffer.channel.entity.Channel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class BasicChannelService implements ChannelService {
+    private final Map<UUID, Channel> channelRepository = new HashMap<>();
+
+    @Override
+    public Channel create(String name, String description) {
+        if(name == null) {
+            throw new IllegalArgumentException("name cannot be null");
+        }
+        if(description == null) {
+            description = "this is " + name + "channel!";
+        }
+
+        Channel channel = new Channel(name, description);
+        channelRepository.put(channel.getId(), channel);
+        return channel;
+    }
 }
--- src/main/java/com/b1uffer/channel/service/ChannelService.java
+++ src/main/java/com/b1uffer/channel/service/ChannelService.java
@@ -1,4 +1,7 @@
 package com.b1uffer.channel.service;
 
+import com.b1uffer.channel.entity.Channel;
+
 public interface ChannelService {
+    Channel create(String name, String description);
 }
