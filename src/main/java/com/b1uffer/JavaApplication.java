c93912ede4c2d25c4317dc67ec0d1e6bf8831512
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-01-10 19:19:52 +0900

    Message 서비스 테스트 로직을 추가합니다.

--- src/main/java/com/b1uffer/JavaApplication.java
+++ src/main/java/com/b1uffer/JavaApplication.java
@@ -3,6 +3,9 @@ package com.b1uffer;
 import com.b1uffer.channel.entity.Channel;
 import com.b1uffer.channel.service.BasicChannelService;
 import com.b1uffer.channel.service.ChannelService;
+import com.b1uffer.message.entity.Message;
+import com.b1uffer.message.service.MessageService;
+import com.b1uffer.message.service.basic.BasicMessageService;
 import com.b1uffer.user.entity.User;
 import com.b1uffer.user.service.UserService;
 import com.b1uffer.user.service.basic.BasicUserService;
@@ -24,8 +27,13 @@ public class JavaApplication {
         channelService.changeChannelTypePrivate(channel.getId(), false);
         channelService.changeChannelTypePublic(channel.getId(), true);
         channelService.delete(channel.getId());
-
-
-        // channel
+        
+        // message
+        MessageService messageService = new BasicMessageService(userService);
+        User messageUser = userService.create("messageUser", "1234", "description", "image");
+        Message message = messageService.create("messageText", messageUser.getId());
+        messageService.read(message.getId());
+        messageService.update(messageUser.getId(), message.getId(), "updatedMessageText");
+        messageService.delete(messageUser.getId());
     }
 }
